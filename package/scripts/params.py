5a75c39af3a980ce415087b2d90a24e4bde94b1a
Chris Gianelloni <chris@cask.co>
2016-07-16 13:48:54 -0400

    CDAP-6512 Remove root.namespace from zookeeper.quorum

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -103,7 +103,7 @@ for i, val in enumerate(zk_hosts):
     zookeeper_hosts += val + ':' + zk_client_port
     if (i + 1) < len(zk_hosts):
         zookeeper_hosts += ','
-cdap_zookeeper_quorum = zookeeper_hosts + '/' + root_namespace
+cdap_zookeeper_quorum = zookeeper_hosts
 
 kafka_bind_port = str(default('/configurations/kafka-broker/port', 6667))
 kafka_hosts = config['clusterHostInfo']['kafka_broker_hosts']
